6cf58d4f0c8b9a5a3926e3f0df21844ec835d1c8
LiamZi <yanzi1221@hotmail.com>
2022-11-15 23:02:24 +0800

    Update resource.h

---
+++ Torch/Core/Src/TorchConfig/resource.h
@@ -0,0 +1,26 @@
+#define IDC_RES_COMBO                   1008
+#define IDC_OUTPUT_COMBO                1009
+#define IDC_PAPER_WHITE_COMBO           1010
+#define IDC_RES_COMBO4                  1011
+#define IDC_MAX_LUM_COMBO               1011
+#define IDC_SYNC_COMBO                  1014
+#define IDC_FACTORY_COMBO               1015
+#define IDC_STEREO_SEP_EDIT             1016
+#define IDC_HDR_COMBO                   1017
+#define IDC_STEREO_COMBO                1018
+#define IDC_CLR_FMT_COMBO               1019
+#define IDC_DEPTH_FMT_COMBO             1020
+#define IDC_AA_COMBO                    1021
+#define IDC_FS_COMBO                    1022
+#define IDC_PPAA_COMBO                  1023
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1018
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
