8ebc0b8a9aa25d6a13b691b4be417f8aecd5cdb7
LiamZi <yanzi1221@hotmail.com>
2022-12-08 23:49:01 +0800

    modify rc

---
+++ Torch/Core/Src/TorchConfig/TorchConfig.cpp
@@ -0,0 +1,15 @@
+#include <Torch/Torch.hpp>
+#include <Windows.h>
+#include <tchar.h>
+#include <stdlib.h>
+#include <commctrl.inl>
+#include <iterator>
+#include <sstream>
+#include <string>
+
+#include "resource.h"
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE , _In_ LPSTR, _In_ int )
+{
+    return 0;
+}
