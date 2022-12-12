fd9c689519400be314d80fb026347a4ba2843855
LiamZi <yanzi1221@hotmail.com>
2022-12-13 00:46:56 +0800

    1. 增加element format

---
+++ Torch/Core/Src/Renderer/ElementFormat.cpp
@@ -0,0 +1,12 @@
+#include <Torch/Torch.hpp>
+#include <Torch/Renderer/ElementFormat.hpp>
+
+#include <glm/glm.hpp>
+
+namespace Torch
+{
+    // void ConvertToABGR32F(ElementFormat fmt, void const * input, uint32_t num_elems, Color* output)
+    // {
+        
+    // }
+}
