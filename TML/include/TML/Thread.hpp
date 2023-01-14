f18530ed85740bc925637a12e4a54de5582479ca
资章明 <zizhangming@sandboxol.com>
2023-01-14 15:22:50 +0800

    1. add thread

---
+++ TML/include/TML/Thread.hpp
@@ -0,0 +1,20 @@
+#ifndef __TML_THREAD_HPP__
+#define __TML_THREAD_HPP__
+
+#pragma once
+
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <mutex>
+#include <thread>
+
+namespace Torch
+{
+    class ThreadPool 
+    {
+        
+    };
+};
+
+#endif
